266881027b22069ac825293f53a3cc5fded4ab10
davideweaver <davideweaver@users.noreply.github.com>
2021-07-01 18:07:00 -0400

    Update hello script

--- hello.ts
+++ hello.ts
@@ -1,8 +1,12 @@
-import { h } from "https://x.lcas.dev/preact@10.5.12/mod.js";
+// We register our function to be called on "fetch" events.
+addEventListener("fetch", (event: FetchEvent) => {
+  // The FetchEvent has a property named `request` which holds
+  // the details of the incoming request and is of type
+  // `Request` of the Fetch API.
+  const method = event.request.method;
 
-addEventListener("fetch", (event) => {
-  const response = new Response("Hello World!", {
-    headers: { "content-type": "text/plain" },
-  });
-  event.respondWith(response);
+  // We call the `respondWith` method to respond to the request.
+  // The argument should either be a `Response` or a promise that
+  // resolves to a Response.
+  event.respondWith(new Response(`Hello. You made a ${method} request.`));
 });
